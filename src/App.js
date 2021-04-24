3b0d0fd7ece746e32262706a5df88f150df0079c
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-04-24 20:37:44 +0600

    new updates

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ class App extends React.Component {
 
   state = {
     datas: [
-      { id: 1, des: "this is list 1" },
+      { id: 3, des: "this is list 1" },
       { id: 2, des: "this is list 2" },
-      { id: 3, des: "this is list 3" },
+      { id: 1, des: "this is list 3" },
     ],
-    data: ""
+    data: "",
+    
   }
+
+  
 
   addTodoList = () => {
     let customdatas = [...this.state.datas];
@@ -20,34 +23,52 @@ class App extends React.Component {
     this.setState({
       datas: customdatas
     })
+
+
+   
   }
 
   getInputValues = (e) => {
+  
     this.setState({
       data: {
-        id: 4, des: e
+        id: this.state.datas.length+1, des: e
       }
     })
   }
 
   deletTodoList = (evnt) => {
     let customdatas = [...this.state.datas];
-    let filterArray = customdatas.filter((e, i) => e.id != evnt);
+    let filterArray = customdatas.filter((e, i) => e.id !== evnt);
     this.setState({
       datas: filterArray
     })
   }
 
+  editTodoList = (eve) => {
+    let customdatas = [...this.state.datas];
+    //let filterArray = customdatas.filter((e, i) => e.id != eve);
+    console.log(customdatas)
+    let editArray = customdatas.map((e, i) => <>{
+      e.id===eve?this.setState({
+        data:e
+      }):null
+    }</> );
+    console.log(editArray);
+  };
 
   render() {
     return (
       <div>
-        <input onChange={event => this.getInputValues(event.target.value)} />
+        <input onChange={event => this.getInputValues(event.target.value)}  />
         <button onClick={() => this.addTodoList()}>Add Button</button>
-        <List datas={this.state.datas} deletTodoListFunc={this.deletTodoList} />
+        <List datas={this.state.datas} deletTodoListFunc={this.deletTodoList} editTodoListFunc={this.editTodoList} />
       </div >
     )
   }
 }
 
 export default App;
+// this.getInputValues(event.target.value)
+
+
--- src/containers/todolist/lists/lists.jsx
+++ src/containers/todolist/lists/lists.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 
-const List = ({ datas, deletTodoListFunc }) => {
+const List = ({ datas, deletTodoListFunc, editTodoListFunc }) => {
     return (
         <>
 
             {/* {console.log(data)} */}
             <ul>
                 {datas.map((e, i) => {
-                    // if (e.id === 3) {
-                    //     return <li key={i}>{e.des}</li>
-                    // }
-                    return <li key={i}>{e.des} <button onClick={() => deletTodoListFunc(e.id)}> X </button></li>
+                    
+                   
+                    return <li key={i}>
+                        {e.des} <button onClick={() => deletTodoListFunc(e.id)}> X </button>
+                        <button onClick={() => editTodoListFunc(e.id)}> Edit  </button>
+                    </li>
                 })}
             </ul>
         </>)
@@ -19,3 +21,4 @@ const List = ({ datas, deletTodoListFunc }) => {
 
 
 export default List;
+
